648718843894c95100c117b6740d323ba8d93b32
John Doe <leblois.erwan@hotmail.fr>
2020-04-06 15:27:32 -0300

    [added][plugin] source-file-system [modifs] header

--- src/components/header.js
+++ src/components/header.js
@@ -1,12 +1,23 @@
 // import { Link } from "gatsby";
-import React from "react";
+// import React from "react";
 // import PropTypes from "prop-types"
 // import Img from "gatsby-image"
 // import { css } from "@emotion/core"
 
-class Header extends React.Component {
-  render() {
-    return (
+import React from "react";
+import { StaticQuery, graphql } from "gatsby";
+
+const Header = () => (
+  <StaticQuery
+    query={graphql`
+      {
+        logo: file(relativePath: { eq: "logo.png" }) {
+          id
+          name
+        }
+      }
+    `}
+    render={(data) => (
       <nav
         className="navbar is-fixed-top"
         role="navigation"
@@ -20,8 +31,30 @@ class Header extends React.Component {
           src={"/logo.png"}
         />
       </nav>
-    );
-  }
-}
+    )}
+  ></StaticQuery>
+);
 
 export default Header;
+
+// class Header extends React.Component {
+//   render() {
+//     return (
+//       <nav
+//         className="navbar is-fixed-top"
+//         role="navigation"
+//         aria-label="main navigation"
+//         style={{ justifyContent: "center" }}
+//       >
+//         <img
+//           alt="logo"
+//           style={{ margin: "auto" }}
+//           className="image is-96x96"
+//           src={"/logo.png"}
+//         />
+//       </nav>
+//     );
+//   }
+// }
+
+// export default Header;
