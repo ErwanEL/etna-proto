c88f2bb29021db1648baadde83c98615b672d733
Erwan EL <leblois.erwan@hotmail.fr>
2020-07-10 16:24:16 +0200

    [proto] snipcart_2, new carrito button

--- src/components/cardProduct.js
+++ src/components/cardProduct.js
@@ -20,8 +20,8 @@ const CardProduct = ({ product }) => {
         style={{ paddingTop: "25%" }}
         sizes={product.image.sizes}
       />
-      <button
-        class="snipcart-add-item"
+      {/* <button
+        class="snipcart-add-item button is-light"
         data-item-id={product.id}
         data-item-price={product.price}
         data-item-url="/paintings/starry-night"
@@ -30,12 +30,26 @@ const CardProduct = ({ product }) => {
         data-item-name={product.name}
       >
         Agregar al carrito
-      </button>
+      </button> */}
       <div className="card-content">
         <p className="title-product is-6">{product.name}</p>
         <span className="price is-size-5">{product.price}$</span>
         {product.stock && <span class="stock tag is-light">Sin stock</span>}
       </div>
+      <footer class="card-footer ">
+        <a
+          href="#"
+          class="card-footer-item snipcart-add-item agregarButton"
+          data-item-id={product.id}
+          data-item-price={product.price}
+          data-item-url="/paintings/starry-night"
+          data-item-description="High-quality replica of The Starry Night by the Dutch post-impressionist painter Vincent van Gogh."
+          data-item-image={product.image.url}
+          data-item-name={product.name}
+        >
+          Agregar al carrito
+        </a>
+      </footer>
     </div>
   );
 };
